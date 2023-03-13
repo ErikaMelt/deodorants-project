ec1d1dbc67ea898b0fff0162cf5855371b05ed38
eomac <104458004+ErikaMelt@users.noreply.github.com>
2023-03-13 11:29:58 +0100

    update project

--- data_cleaning/utils.py
+++ data_cleaning/utils.py
@@ -1,12 +1,11 @@
-import pandas as pd
 from google.cloud import storage
-from google.cloud.exceptions import NotFound, Forbidden
+from google.cloud.exceptions import Forbidden
 from google.cloud import bigquery
 from google.cloud.exceptions import NotFound, BadRequest
 from google.oauth2.service_account import Credentials
 import time
 import pandas as pd
-
+import datetime
 
 def convert_file(df, col_name, data_type, date_format="%Y%m%d", col_names=None):
     """
@@ -137,51 +136,19 @@ def get_data_big_query():
         print(f'An error occurred while retrieving data from BigQuery: {e}')
 
 
-def get_big_query_data(page_size=200000):
-    try:
-        key_path = 'deodorants-7c413894015d.json'
-        credentials = Credentials.from_service_account_file(key_path)
-        client = bigquery.Client(credentials=credentials)
+def get_season(date):
+    year = date.year
+    seasons = [('invierno', datetime.date(year, 12, 21), datetime.date(year + 1, 3, 20)),
+               ('primavera', datetime.date(year, 3, 21), datetime.date(year, 6, 20)),
+               ('verano', datetime.date(year, 6, 21), datetime.date(year, 9, 20)),
+               ('otono', datetime.date(year, 9, 21), datetime.date(year, 12, 20))]
+    for season, start_date, end_date in seasons:
+        if start_date <= pd.Timestamp(date) <= end_date:
+            return season
+    return 'invierno'
 
-        sql_template = """
-        SELECT *
-        FROM `deodorants.deodorants_trans.deodorants_merged` 
-        ORDER BY fecha_trans, idb, id_producto
-        LIMIT {page_size}
-        OFFSET {offset}
-        """
 
-        # Start the first query job
-        offset = 0
-        query_job = client.query(sql_template.format(page_size=page_size, offset=offset))
 
-        # Read the data in chunks and concatenate the results
-        df_list = []
-        while True:
-            for i, row in enumerate(query_job.result()):
-                row_dict = dict(row.items())
-                df_list.append(pd.DataFrame(row_dict, index=[i]))
-                print(f'Reading chunk {len(df_list)}')
-            if query_job.next_page_token is None:
-                break
-            else:
-                offset += page_size
-                query_job = client.query(sql_template.format(page_size=page_size, offset=offset),
-                                         job_config=bigquery.QueryJobConfig(
-                                             use_query_cache=False,
-                                             maximum_bytes_billed=10 * 1024 * 1024 * 1024
-                                             # set the maximum bytes billed to 10GB
-                                         ))
-
-        df = pd.concat(df_list)
-        return df
-
-    except NotFound as e:
-        print(f'BigQuery table not found: {e}')
-    except BadRequest as e:
-        print(f'Invalid BigQuery request: {e}')
-    except Exception as e:
-        print(f'An error occurred while retrieving data from BigQuery: {e}')
 
 
 
